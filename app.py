5de84c4bb8f51b1262c84666cc4df324fe22697a
Misgar <renato.oliveira34@fatec.sp.gov.br>
2025-05-11 19:07:56 -0300

    Apos importação do tKinter, realizados os primeiros testes com parametros para a interface grafica

---
+++ app.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+
+
+root = tk.Tk()
+
+
+root.title("Temperature Catcher - São Paulo")
+root.configure(background="gray")
+root.minsize(200, 200)
+root.maxsize(500, 500)
+root.geometry("300x300+50+50")
+
+
+root.mainloop()
