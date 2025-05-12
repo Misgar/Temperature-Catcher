2667068edd9ad3f0cbeba7864b65ca8751e4eb4f
Misgar <renato.oliveira34@fatec.sp.gov.br>
2025-05-11 23:29:19 -0300

    Adicionado retorno de quantidade de queimadas realizadas proximas da região SP. Corrigido botão que salva na planilha

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import tkinter as tk
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -12,7 +13,7 @@ import time
 
 
 
-arquivoExcel = "registro-temperaturas.xlsx"  # Arquivo Excel onde serão salvos os dados
+arquivoExcel = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'temperaturas.xlsx')  # Arquivo Excel onde serão salvos os dados
 
 root = tk.Tk()
 pointer = 1
@@ -26,16 +27,19 @@ root.geometry("100x100+25+25")
 
 tk.Label(root, text = "Atualizar Previsão na planilha")
 
-curTime=datetime.now()
+curTime =datetime.now() # Salvando data e hora na variavel curTime
+curTimeFormated = curTime.strftime("%Y-%m-%d %H:%M:%S")
+
+
 
 def buttonPressed():
-   temperatura = obter_temperatura_sao_paulo()
-   salvar_temperatura_em_excel(arquivoExcel, temperatura)
+    temperatura, qtdQueimadas = obter_temperatura_sao_paulo()
+    salvar_temperatura_em_excel(arquivoExcel, temperatura, qtdQueimadas)
+    
 
 
-
-# Função responsavel por retornar a temperatura no site CLima Tempo
-def obter_temperatura_sao_paulo():
+# Função responsavel por retornar a temperatura no site CLima Tempo. utilização do selenium
+def obter_temperatura_sao_paulo():  
     options = Options()
     options.add_argument("--headless") ## COM ESSE ARGUMENTO, PODEMOS RODAR SEM ABRIR O NAVEGADOR. OPCIONAL 
     options.add_argument("--disable-gpu") ## COM ESSE ARGUMENTO, PODEMOS RODAR SEM ABRIR O NAVEGADOR. OPCIONAL 
@@ -50,31 +54,35 @@ def obter_temperatura_sao_paulo():
         temperatura = WebDriverWait(driver, 5).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "span.-bold.-gray-dark-2.-font-55._margin-l-20._center"))).text
         
+        queimadas = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.ID, "recentFire"))).text
+        
         #retorno do valor da temperatura para usar n a função intermediaria (que chama as duas.)
-        return temperatura
+        return temperatura, queimadas
     finally:
         driver.quit()
 
 
 
-def salvar_temperatura_em_excel(caminho_arquivo, temperatura):
+def salvar_temperatura_em_excel(caminho_arquivo, temperatura, queimadas):
     # Abre a planilha existente
     planilhaCaminho = load_workbook(caminho_arquivo)
     planilhaAba = planilhaCaminho.active  # Usa a primeira aba (sheet)
 
     # Encontra a próxima linha vazia
-    proxima_linha = planilhaCaminho.max_row + 1
+    proxima_linha = planilhaAba.max_row + 1
 
     # Insere a data/hora e a temperatura
     planilhaAba.cell(row=proxima_linha, column=1, value=curTime) # CurTime esta sendo definida acima, e mostra a data e hora no bottom da interface
     planilhaAba.cell(row=proxima_linha, column=2, value=temperatura)
+    planilhaAba.cell(row=proxima_linha, column=3, value=queimadas)
 
-    # Salva o arquivo
-    planilhaAba.save(caminho_arquivo)
-    print(f"Temperatura {temperatura}°C salva na linha {proxima_linha}.")
+    # Salva o arquivo e exibe no terminal
+    planilhaCaminho.save(caminho_arquivo)
+    print(f"Temperatura {temperatura}°C salva na linha {proxima_linha}.") # Aqui confirmamos no terminal se a temperatura foi salva, e em qual linha.
 
 
-tk.Label(root, text=curTime).pack(side=tk.BOTTOM)
+tk.Label(root, text=curTimeFormated).pack(side=tk.BOTTOM) #Salvando data e hora no final da janela
 
 
 tk.Label(root, text='Buscar dados de Temperatura').pack()
